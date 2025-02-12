b1521c56f67c2258653f6ce82bba444edb3aece7
barbarakesel <kesel2004@mail.ru>
2025-02-12 10:42:17 +0300

    Initial commit

---
+++ process.php
@@ -0,0 +1,26 @@
+<?php
+$host = "localhost";
+$user = "root";
+$password = "";
+$dbname = "user_database";
+
+$conn = new mysqli($host, $user, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+//запись данных в базу данных
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $firstName = $conn->real_escape_string($_POST['firstName']);
+    $lastName = $conn->real_escape_string($_POST['lastName']);
+    $email = $conn->real_escape_string($_POST['email']);
+
+    $sql = "INSERT INTO users (first_name, last_name, email) VALUES ('$firstName', '$lastName', '$email')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Данные успешно сохранены!";
+    } else {
+        echo "Ошибка: " . $conn->error;
+    }
+}
+
+$conn->close();
